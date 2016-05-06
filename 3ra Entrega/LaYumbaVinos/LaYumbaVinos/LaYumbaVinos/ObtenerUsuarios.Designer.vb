336dbc84f70b475116f964031aa078909ea8ed06
pssettino <pablo.settino@gmail.com>
2016-05-05 21:32:36 -0300

    Inicio de 2da entrega
    .

--- 3ra Entrega/LaYumbaVinos/LaYumbaVinos/LaYumbaVinos/ObtenerUsuarios.Designer.vb
+++ 3ra Entrega/LaYumbaVinos/LaYumbaVinos/LaYumbaVinos/ObtenerUsuarios.Designer.vb
@@ -24,39 +24,109 @@ Partial Class ObtenerUsuarios
     Private Sub InitializeComponent()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
         Me.Usuarios = New System.Windows.Forms.GroupBox()
-        Me.Usuario = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Id = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.DNI = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ApeNom = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.Telefono = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.Email = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.ModificarUsuario = New System.Windows.Forms.DataGridViewButtonColumn()
         Me.Eliminar = New System.Windows.Forms.DataGridViewButtonColumn()
-        Me.Bloquear = New System.Windows.Forms.DataGridViewButtonColumn()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.Usuarios.SuspendLayout()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Usuario, Me.ApeNom, Me.Email, Me.ModificarUsuario, Me.Eliminar, Me.Bloquear})
-        Me.DataGridView1.Location = New System.Drawing.Point(17, 30)
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.Id, Me.DNI, Me.ApeNom, Me.Telefono, Me.Email, Me.ModificarUsuario, Me.Eliminar})
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 18)
         Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(739, 205)
+        Me.DataGridView1.Size = New System.Drawing.Size(859, 123)
         Me.DataGridView1.TabIndex = 0
         '
         'Usuarios
         '
         Me.Usuarios.Controls.Add(Me.DataGridView1)
-        Me.Usuarios.Location = New System.Drawing.Point(12, 3)
+        Me.Usuarios.Location = New System.Drawing.Point(12, 112)
         Me.Usuarios.Name = "Usuarios"
-        Me.Usuarios.Size = New System.Drawing.Size(762, 260)
+        Me.Usuarios.Size = New System.Drawing.Size(873, 151)
         Me.Usuarios.TabIndex = 1
         Me.Usuarios.TabStop = False
-        Me.Usuarios.Text = "Usuarios"
+        Me.Usuarios.Text = "Clientes"
         '
-        'Usuario
+        'GroupBox1
         '
-        Me.Usuario.HeaderText = "Usuario"
-        Me.Usuario.Name = "Usuario"
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.TextBox2)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 6)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(873, 100)
+        Me.GroupBox1.TabIndex = 2
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Buscar"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(666, 43)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(100, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Buscar"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(47, 45)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(121, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(329, 45)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(307, 20)
+        Me.TextBox2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(15, 49)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(26, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "DNI"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(231, 49)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Apellido y Nombre"
+        '
+        'Id
+        '
+        Me.Id.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
+        Me.Id.HeaderText = "Id"
+        Me.Id.Name = "Id"
+        Me.Id.Width = 41
+        '
+        'DNI
+        '
+        Me.DNI.HeaderText = "DNI"
+        Me.DNI.Name = "DNI"
         '
         'ApeNom
         '
@@ -64,8 +134,16 @@ Partial Class ObtenerUsuarios
         Me.ApeNom.HeaderText = "Apellido y Nombre"
         Me.ApeNom.Name = "ApeNom"
         '
+        'Telefono
+        '
+        Me.Telefono.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader
+        Me.Telefono.HeaderText = "Telefono"
+        Me.Telefono.Name = "Telefono"
+        Me.Telefono.Width = 74
+        '
         'Email
         '
+        Me.Email.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill
         Me.Email.HeaderText = "Email"
         Me.Email.Name = "Email"
         '
@@ -79,31 +157,35 @@ Partial Class ObtenerUsuarios
         Me.Eliminar.HeaderText = ""
         Me.Eliminar.Name = "Eliminar"
         '
-        'Bloquear
-        '
-        Me.Bloquear.HeaderText = ""
-        Me.Bloquear.Name = "Bloquear"
-        Me.Bloquear.Text = ""
-        '
         'ObtenerUsuarios
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(780, 275)
+        Me.ClientSize = New System.Drawing.Size(889, 275)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Usuarios)
         Me.Name = "ObtenerUsuarios"
-        Me.Text = "Usuarios"
+        Me.Text = "Clientes"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.Usuarios.ResumeLayout(False)
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
     Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
-    Friend WithEvents Usuario As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Usuarios As System.Windows.Forms.GroupBox
+    Friend WithEvents Id As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents DNI As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents ApeNom As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents Telefono As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents Email As System.Windows.Forms.DataGridViewTextBoxColumn
     Friend WithEvents ModificarUsuario As System.Windows.Forms.DataGridViewButtonColumn
     Friend WithEvents Eliminar As System.Windows.Forms.DataGridViewButtonColumn
-    Friend WithEvents Bloquear As System.Windows.Forms.DataGridViewButtonColumn
-    Friend WithEvents Usuarios As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 End Class
